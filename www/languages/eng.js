77bfacc25efa5c2cf38925e6b132530eea6a1d3b
Bence Kovacs <kov.benc@gmail.com>
2017-03-08 15:01:36 +0100

    first commit

---
+++ www/languages/eng.js
@@ -0,0 +1,24 @@
+
+txt_kilepes = 'Swipe UP 3 times with 2 fingers to exit!';
+txt_kilep = 'If you want to exit';
+txt_ujra = 'Show again next time';
+txt_megertette = 'OK, I got it';
+
+txt_mitakar = 'What do you want to do?';
+txt_mit_valaszt = 'Please select';
+txt_mit_kilep = 'EXIT TO MENU';
+txt_mit_folyt = 'Continue';
+txt_mit_ujat = 'Restart';
+
+txt_cime2 = 'Skale DEMO';
+txt_alcim = 'app by Benke Smith';
+txt_start_magic = 'START DEMO';
+
+txt_be = 'ON';
+txt_ki = 'OFF';
+
+txt_nemelerheto = 'Skale not found...';
+txt_kapcsolodva = 'CONNECTED';
+txt_kapcsolodik = 'connecting...';
+txt_keres = 'searching...';
+
